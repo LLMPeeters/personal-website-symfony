e48ea78b18e8298a7221b8bb084b2d04efc731ee
LLMPeeters <llmpeeters@gmail.com>
2022-07-02 17:21:27 +0000

    adding login

---
+++ src/Controller/Admin/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    #[IsGranted('ROLE_USER')]
+    #[Route('/admin', name: 'app_admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
