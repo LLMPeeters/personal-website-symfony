f6e6bc1412476cb834d9571144298bf67b1c45e7
LLMPeeters <llmpeeters@gmail.com>
2022-07-20 14:28:29 +0000

    you can now properly add and remove rows of text in a complex page, when creating a new page

--- assets/scripts/sf_collection_type.js
+++ assets/scripts/sf_collection_type.js
@@ -4,18 +4,15 @@ class CollectionForm {
     addButton = null;
     fieldCount = 0;
 	clone = null;
+	fields = [];
 
     constructor(target, attribute) {
         this.prototype = target.getAttribute(attribute);
 		target.removeAttribute(attribute);
-		
         this.target = target;
-
 		this.target.innerHTML = this.prototype;
 		
-		this.clone = this.target.querySelector(`:scope > *`).cloneNode(true);
-		
-		this.setIndex(this.target.querySelector(`:scope > *`));
+		this.initFirstField(this.target.querySelector(`:scope > *`));
 		
 		this.createButtons();
 		
@@ -28,6 +25,7 @@ class CollectionForm {
 		const upButton = document.createElement(`button`);
 		const downButton = document.createElement(`button`);
 		const target = targetToMove;
+		const thisHelper = this;
 		
 		upButton.innerText = `up`;
 		downButton.innerText = `down`;
@@ -40,6 +38,7 @@ class CollectionForm {
 			
 			if(index > 0) {
 				children[index-1].insertAdjacentElement(`beforebegin`, target);
+				thisHelper.resetIndexes();
 			}
 		});
 		
@@ -51,14 +50,15 @@ class CollectionForm {
 			
 			if(index < children.length-1) {
 				children[index+1].insertAdjacentElement(`afterend`, target);
+				thisHelper.resetIndexes();
 			}
 		});
 		
 		return [upButton, downButton];
 	}
 	
-	getNewField() {
-		const field = this.clone.cloneNode(true);
+	getNewField(inputField = null) {
+		const field = inputField ?? this.clone.cloneNode(true);
 		const removeButton = this.createRemoveButton(field);
 		const moveButtons = this.createMoveButtons(field);
 		
@@ -68,8 +68,34 @@ class CollectionForm {
 		
 		this.setIndex(field);
 		field.insertAdjacentElement(`beforeend`, removeButton);
+		this.fields.push(field);
 		
-		return this.setIndex(field);
+		return field;
+	}
+	
+	initFirstField(field) {
+		const elements = [field];
+		
+		for(const item of elements) {
+			for(const child of item.querySelectorAll(`:scope > *`)) {
+				elements.push(child);
+			}
+			
+			const attributeNames = item.getAttributeNames();
+			
+			for(const name of attributeNames) {
+				if(item.getAttribute(name).includes(`__name__`)) {
+					item.setAttribute(
+						`old.${name}`,
+						item.getAttribute(name)
+					)
+				}
+			}
+		}
+		
+		this.clone = field.cloneNode(true);
+		
+		this.getNewField(field);
 	}
 	
 	createAddButton() {
@@ -129,8 +155,18 @@ class CollectionForm {
 		this.addButton = addButton;
 		this.deleteButton = deleteButton;
 	}
+	
+	resetIndexes() {
+		const fields = this.fields[0].parentNode.querySelectorAll(`:scope > *`);
+		
+		this.fieldCount = 0;
+		
+		for(const field of fields) {
+			this.setIndex(field, true);
+		}
+	}
 
-    setIndex(element) {
+    setIndex(element, reset = null) {
 		const elements = [element];
 		
 		for(const item of elements) {
@@ -141,7 +177,16 @@ class CollectionForm {
 			const attributeNames = item.getAttributeNames();
 			
 			for(const name of attributeNames) {
-				if(item.getAttribute(name).includes(`__name__`)) {
+				if(reset) {
+					const oldName = `old.${name}`;
+					const oldValue = item.getAttribute(oldName);
+					
+					if(oldValue) {
+						item.setAttribute(name, oldValue);
+					}
+				}
+				
+				if(name.slice(0, 4) !== `old.` && item.getAttribute(name).includes(`__name__`)) {
 					item.setAttribute(
 						name,
 						item.getAttribute(name).replaceAll(`__name__`, this.fieldCount)
@@ -151,6 +196,7 @@ class CollectionForm {
 		}
         
 		this.fieldCount++;
+		
         return element;
     }
 }
