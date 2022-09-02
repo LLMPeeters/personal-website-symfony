66236c495592e476c3f9c60ffbe268c218723d27
LLMPeeters <llmpeeters@gmail.com>
2022-09-02 12:52:40 +0000

    10: Everything should be working now, I'll be testing it all now

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -8,6 +8,7 @@ use App\Entity\AbstractPage;
 use App\Service\NavDataFinder;
 use App\Entity\ComplexPageData;
 use App\Service\PagesToSitemap;
+use App\Service\RouteValidifier;
 use App\Entity\SupportedLanguage;
 use App\Entity\SimpleTextPageData;
 use App\Component\Pages\PageTypeEnum;
@@ -16,7 +17,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Component\Pages\ComplexPageItemsEnum;
 use Symfony\Component\HttpFoundation\Response;
-use App\Repository\SupportedLanguageRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Component\Config\ForbiddenRoutePrefixesEnum;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -25,22 +25,16 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 class PortfolioController extends AbstractController
 {
     #[Route('/{route}', name: 'app_portfolio', requirements: ['route' => '%app.allowed_routes_regex%'])]
-    public function index(string $route, HotlinkRepository $hRepo, ManagerRegistry $doctrine, PagesToSitemap $pagesToSitemap, SupportedLanguageRepository $slRepo, NavDataFinder $navDataFinder): Response
+    public function index(RouteValidifier $rValidifier, string $route, HotlinkRepository $hRepo, ManagerRegistry $doctrine, PagesToSitemap $pagesToSitemap, NavDataFinder $navDataFinder): Response
     {
 		// A route always starts with a two letter country code with a slash; en/, nl/
         $hotlink = $hRepo->findOneBy(['route' => substr($route, 3)]);
         
-        if($hotlink instanceof Hotlink && is_subclass_of($hotlink->getPageNamespace(), AbstractPage::class)) {
+        if(($lang = $rValidifier->getValidLang($route)) && $hotlink instanceof Hotlink && is_subclass_of($hotlink->getPageNamespace(), AbstractPage::class)) {
             $repo = $doctrine->getRepository($hotlink->getPageDataNamespace());
             $pageData = $repo->findOneBy(['hotlink' => $hotlink]);
-			$supportedLanguage = $slRepo->findOneBy(['countryCode' => substr($route, 0, 2)]);
-			
-			if(!$supportedLanguage instanceof SupportedLanguage) {
-				throw new \Exception('Language not found!');
-			}
-			
-            $navPages = $navDataFinder->getNavData($supportedLanguage);
-			$sitemapData = $pagesToSitemap->getSitemapArray($supportedLanguage);
+            $navPages = $navDataFinder->getNavData($lang);
+			$sitemapData = $pagesToSitemap->getSitemapArray($lang);
 			$renderData = [
 				'page_data' => $pageData,
 				'nav_data' => $navPages,
@@ -54,8 +48,12 @@ class PortfolioController extends AbstractController
 			} else {
 				throw new \LogicException('Unrecognized page type.');
 			}
-        } else {
-			throw new NotFoundHttpException('Page not found.');
-		}
+		} elseif(!$rValidifier->isLanguageValid($route)) {
+			if(is_string($url = $rValidifier->getValidRedirect($route))) {
+				return $this->redirect($url);
+			}
+        }
+		
+		throw new NotFoundHttpException('Page not found.');
     }
 }
--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -6,10 +6,16 @@ use Twig\TwigFunction;
 use App\Entity\ProjectWidget;
 use App\Entity\AbstractWidget;
 use App\Entity\ProgressWidget;
+use App\Service\RouteValidifier;
+use App\Entity\ProjectWidgetData;
+use App\Entity\AbstractWidgetData;
+use App\Entity\ProgressWidgetData;
 use Twig\Extension\AbstractExtension;
 use App\Component\Config\LanguagesEnum;
+use App\Component\Widgets\WidgetTypeEnum;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Component\Pages\ComplexPageItemsEnum;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class AppExtension extends AbstractExtension
 {
@@ -19,6 +25,8 @@ class AppExtension extends AbstractExtension
 		private ManagerRegistry $doctrine,
 		private string $publicProjectsDir,
 		private string $publicImageDir,
+		private RouteValidifier $rValidifier,
+		private RequestStack $requestStack
 	) {}
 	
 	public function getFunctions()
@@ -55,14 +63,32 @@ class AppExtension extends AbstractExtension
 		return LanguagesEnum::tryFromName($code)?->value ?? 'Unknown';
 	}
 	
-	public function getWidget(string $serializedData): false|AbstractWidget
+	public function getWidget(string $serializedData): false|AbstractWidgetData
 	{
 		try {
 			$data = unserialize($serializedData);
-			$widget = $this->doctrine->getRepository($data['entityName'])->find($data['entityId']);
+			$widget = WidgetTypeEnum::tryFrom($data['entityName']);
 			
-			return $widget ?? false;
+			if(!$widget instanceof WidgetTypeEnum) {
+				return false;
+			}
+			
+			$lang = $this->rValidifier->getValidLang($this->requestStack->getCurrentRequest()->getRequestUri());
+			$dataName = $widget->getDataType();
+			$repo = $this->doctrine->getRepository($dataName);
+			$qb = $repo->createQueryBuilder('data')
+				->join('data.widget', 'widget')
+				->select('data')
+				->andWhere('data.supportedLanguage = :lang')
+				->setParameter('lang', $lang)
+				->andWhere('widget.id = :widgetId')
+				->setParameter('widgetId', $data['entityId']);
+			$query = $qb->getQuery();
+			$widgetData = $query->getResult();
+			
+			return $widgetData[0] instanceof AbstractWidgetData ? $widgetData[0] : false;
 		} catch(\Exception $e) {
+			dd($e->getMessage());
 			return false;
 		}
 	}
@@ -91,11 +117,11 @@ class AppExtension extends AbstractExtension
 	
 	public function isProgressWidget(mixed $widget): bool
 	{
-		return $widget instanceof ProgressWidget;
+		return $widget instanceof ProgressWidgetData;
 	}
 	
 	public function isProjectWidget(mixed $widget): bool
 	{
-		return $widget instanceof ProjectWidget;
+		return $widget instanceof ProjectWidgetData;
 	}
 }
