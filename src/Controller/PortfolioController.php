9115b66eb0b69b4f42d1cf3a91c9c944d8758157
LLMPeeters <llmpeeters@gmail.com>
2022-09-05 13:22:34 +0000

    11: Everything should be workable for me

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -8,6 +8,7 @@ use App\Entity\AbstractPage;
 use App\Service\NavDataFinder;
 use App\Entity\ComplexPageData;
 use App\Service\PagesToSitemap;
+use App\Entity\AbstractPageData;
 use App\Form\LanguageSelectType;
 use App\Service\RouteValidifier;
 use App\Entity\SupportedLanguage;
@@ -19,6 +20,7 @@ use Doctrine\Persistence\ManagerRegistry;
 use App\Component\Pages\ComplexPageItemsEnum;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\SupportedLanguageRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Component\Config\ForbiddenRoutePrefixesEnum;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -38,44 +40,44 @@ class PortfolioController extends AbstractController
 		HotlinkRepository $hRepo,
 		ManagerRegistry $doctrine,
 		PagesToSitemap $pagesToSitemap,
-		NavDataFinder $navDataFinder
+		NavDataFinder $navDataFinder,
+		SupportedLanguageRepository $slRepo
 	): Response {
-		// A route always starts with a two letter country code with a slash; en/, nl/
-        $hotlink = $hRepo->findOneBy(['route' => substr($route, 3)]);
-        
-        if(($lang = $rValidifier->getValidLang($route)) && $hotlink instanceof Hotlink && is_subclass_of($hotlink->getPageNamespace(), AbstractPage::class)) {
-			// $form = $this->createForm(LanguageSelectType::class, []);
-			// $form->handleRequest($request);
+		/**
+		 * First find a valid SupportedLanguage
+		 * Then find a valid Hotlink by using the route and SupportedLanguage
+		 * Then find the AbstractPageData that has a (fake) OneToOne relation with Hotlink
+		 */
+        if(($lang = $rValidifier->getValidLang($route))) {
+			$hotlink = $hRepo->findOneBy([
+				'route' => substr($route, 3),
+				'supportedLanguage' => $lang
+			]);
 			
-			// if($form->isSubmitted() && $form->isValid()) {
-			// 	dd($form);
+			if($hotlink instanceof Hotlink) {
+				$repo = $doctrine->getRepository($hotlink->getPageDataNamespace());
+				$pageData = $repo->findOneBy(['hotlink' => $hotlink]);
+				$navPages = $navDataFinder->getNavData($lang);
+				$sitemapData = $pagesToSitemap->getSitemapArray($lang);
+				$renderData = [
+					'page_data' => $pageData,
+					'nav_data' => $navPages,
+					'sitemap_data' => $sitemapData,
+				];
 				
-			// 	// TODO: redirect to current page in the selected language
-			// }
-			
-            $repo = $doctrine->getRepository($hotlink->getPageDataNamespace());
-            $pageData = $repo->findOneBy(['hotlink' => $hotlink]);
-            $navPages = $navDataFinder->getNavData($lang);
-			$sitemapData = $pagesToSitemap->getSitemapArray($lang);
-			$renderData = [
-				'page_data' => $pageData,
-				'nav_data' => $navPages,
-				'sitemap_data' => $sitemapData,
-				// 'lang_select_form' => $form->createView(),
-			];
-			
-            if($pageData instanceof SimpleTextPageData) {
-                return $this->render('portfolio/simple_page.html.twig', $renderData);
-            } elseif($pageData instanceof ComplexPageData) {
-				return $this->render('portfolio/complex_page.html.twig', $renderData);
-			} else {
-				throw new \LogicException('Unrecognized page type.');
+				if($pageData instanceof AbstractPageData) {
+					if($pageData instanceof SimpleTextPageData) {
+						return $this->render('portfolio/simple_page.html.twig', $renderData);
+					} elseif($pageData instanceof ComplexPageData) {
+						return $this->render('portfolio/complex_page.html.twig', $renderData);
+					} else {
+						throw new \LogicException('Unrecognized page type.');
+					}
+				}
 			}
-		} elseif(!$rValidifier->isLanguageValid($route)) {
-			if(is_string($url = $rValidifier->getValidRedirect($route))) {
-				return $this->redirect($url);
-			}
-        }
+		} elseif($url = $rValidifier->getValidRedirect($route)) {
+			return $this->redirect($url);
+		}
 		
 		throw new NotFoundHttpException('Page not found.');
     }
